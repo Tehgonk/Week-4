c934c42bb5dfc55298767f547824e3c4efd82bf7
D Geula <gonkhead112@gmail.com>
2022-06-05 16:22:09 +0100

    Initial commit

---
+++ Exercise 1/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+double energy(double m); // function prototype.
+
+int main()
+{
+    double mass;
+
+    printf("\nEnter mass [Kg]: ");
+    scanf("%lf", &mass);
+
+    printf("\nEnergy = %lf Joules", energy(mass)); // passes the value of 'mass` to `energy(m)`.
+
+    return 0;
+}
+
+double energy(double m) // function definition for prototype.
+{
+    double e;
+    const double c = 299792458; // in m/s. (approx 3*10^8 or 3e8).
+
+    e = m * pow(c, 2);
+
+    return e; // returns the value of `e` with type `double`.
+}
