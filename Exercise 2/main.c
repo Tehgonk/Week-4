7391c672a484537e770898c80797248c6d0a8c53
D Geula <gonkhead112@gmail.com>
2022-06-05 22:35:51 +0100

    Initial commit

---
+++ Exercise 2/main.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <math.h>
+
+const double c = 299792458; // speed of light in m/s
+
+double timeDilation(double v, double t0);
+
+int main()
+{
+    double timeInterval; // time interval in seconds as measured by the stationary observer
+    double velocity; // velocity of observer
+
+    printf("\nTime interval [seconds]: ");
+    scanf("%lf", &timeInterval);
+
+    printf("\nObserver velocity [as a fraction of C]: ");
+    scanf("%lf", &velocity);
+
+    velocity *= c; // convert to m/s
+
+    printf("\n\nRelative time is %lf hours, %lf minutes, or %lf seconds.\n", timeDilation(timeInterval, velocity) / 3600, timeDilation(timeInterval, velocity) / 60, timeDilation(timeInterval, velocity));
+
+    return 0;
+}
+
+double timeDilation(double t_0, double v)
+{
+    double t_1; // relative time in seconds
+
+    t_1 = t_0 / sqrt(1 - (pow(v, 2) / pow(c, 2)));
+
+    return t_1;
+}
